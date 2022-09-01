2e3e38cb5b22d760f77feb81f0e0a8dec183f485
Axel Yataco <axelyataco1998@gmail.com>
2022-09-01 01:31:32 -0500

    Sección 4: Getting started with Cypress Test Automation

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,6 +1,9 @@
-describe('', () => {
-    it('', () => {
-        
+describe('My first Test Suite', () => {
+    it('My first test case', () => {
+        cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
+        cy.get('.search-keyword').type('ca');
+        cy.wait(2000);
+        cy.get('.product:visible').should('have.length',4); // :visible sirve para que solo tome en cuenta elementos visibles. En este caso que la lista de .product sea 4 visibles
         
     });
 });
